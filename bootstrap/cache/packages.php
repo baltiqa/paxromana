5a2625358f7889502488e0d143cd7fc000755f68
RaresMihut <63499383+RaresMihut@users.noreply.github.com>
2020-04-11 11:33:31 +0000

    Add files via upload

---
+++ bootstrap/cache/packages.php
@@ -0,0 +1,241 @@
+<?php return array (
+  'amelia/backblaze' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Amelia\\Backblaze\\BackblazeServiceProvider',
+    ),
+  ),
+  'anlutro/l4-settings' => 
+  array (
+    'aliases' => 
+    array (
+      'Setting' => 'anlutro\\LaravelSettings\\Facade',
+    ),
+    'providers' => 
+    array (
+      0 => 'anlutro\\LaravelSettings\\ServiceProvider',
+    ),
+  ),
+  'atayahmet/laravel-nestable' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Nestable\\NestableServiceProvider',
+    ),
+    'aliases' => 
+    array (
+      'Nestable' => 'Nestable\\Facades\\NestableService',
+    ),
+  ),
+  'barryvdh/laravel-debugbar' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Barryvdh\\Debugbar\\ServiceProvider',
+    ),
+    'aliases' => 
+    array (
+      'Debugbar' => 'Barryvdh\\Debugbar\\Facade',
+    ),
+  ),
+  'chumper/zipper' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Chumper\\Zipper\\ZipperServiceProvider',
+    ),
+    'aliases' => 
+    array (
+      'Zipper' => 'Chumper\\Zipper\\Zipper',
+    ),
+  ),
+  'cviebrock/eloquent-sluggable' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Cviebrock\\EloquentSluggable\\ServiceProvider',
+    ),
+  ),
+  'cybercog/laravel-ban' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Cog\\Laravel\\Ban\\Providers\\BanServiceProvider',
+    ),
+  ),
+  'grimzy/laravel-mysql-spatial' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Grimzy\\LaravelMysqlSpatial\\SpatialServiceProvider',
+    ),
+  ),
+  'igaster/laravel-theme' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Igaster\\LaravelTheme\\themeServiceProvider',
+    ),
+    'aliases' => 
+    array (
+      'Theme' => 'Igaster\\LaravelTheme\\Facades\\Theme',
+    ),
+  ),
+  'intervention/image' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Intervention\\Image\\ImageServiceProvider',
+    ),
+    'aliases' => 
+    array (
+      'Image' => 'Intervention\\Image\\Facades\\Image',
+    ),
+  ),
+  'ixudra/curl' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Ixudra\\Curl\\CurlServiceProvider',
+    ),
+    'aliases' => 
+    array (
+      'Curl' => 'Ixudra\\Curl\\Facades\\Curl',
+    ),
+  ),
+  'jpmurray/laravel-countdown' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'jpmurray\\LaravelCountdown\\CountdownServiceProvider',
+    ),
+    'aliases' => 
+    array (
+      'Countdown' => 'jpmurray\\LaravelCountdown\\Facades\\CountdownFacade',
+    ),
+  ),
+  'kris/laravel-form-builder' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Kris\\LaravelFormBuilder\\FormBuilderServiceProvider',
+    ),
+    'aliases' => 
+    array (
+      'FormBuilder' => 'Kris\\LaravelFormBuilder\\Facades\\FormBuilder',
+    ),
+  ),
+  'laravel/tinker' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Laravel\\Tinker\\TinkerServiceProvider',
+    ),
+  ),
+  'laravelcollective/html' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Collective\\Html\\HtmlServiceProvider',
+    ),
+    'aliases' => 
+    array (
+      'Form' => 'Collective\\Html\\FormFacade',
+      'Html' => 'Collective\\Html\\HtmlFacade',
+    ),
+  ),
+  'nahid/talk' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Nahid\\Talk\\TalkServiceProvider',
+    ),
+    'aliases' => 
+    array (
+      'Talk' => 'Nahid\\Talk\\Facades\\Talk',
+    ),
+  ),
+  'nunomaduro/collision' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'NunoMaduro\\Collision\\Adapters\\Laravel\\CollisionServiceProvider',
+    ),
+  ),
+  'overtrue/laravel-follow' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Overtrue\\LaravelFollow\\FollowServiceProvider',
+    ),
+  ),
+  'rcrowe/twigbridge' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'TwigBridge\\ServiceProvider',
+    ),
+    'aliases' => 
+    array (
+      'Twig' => 'TwigBridge\\Facade\\Twig',
+    ),
+  ),
+  'simplesoftwareio/simple-qrcode' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'SimpleSoftwareIO\\QrCode\\QrCodeServiceProvider',
+    ),
+    'aliases' => 
+    array (
+      'QrCode' => 'SimpleSoftwareIO\\QrCode\\Facades\\QrCode',
+    ),
+  ),
+  'sofa/eloquence-base' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Sofa\\Eloquence\\BaseServiceProvider',
+    ),
+  ),
+  'sofa/eloquence-mutable' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Sofa\\Eloquence\\MutableServiceProvider',
+    ),
+  ),
+  'spatie/laravel-http-logger' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Spatie\\HttpLogger\\HttpLoggerServiceProvider',
+    ),
+  ),
+  'spatie/laravel-tags' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Spatie\\Tags\\TagsServiceProvider',
+    ),
+  ),
+  'spatie/laravel-translatable' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Spatie\\Translatable\\TranslatableServiceProvider',
+    ),
+  ),
+  'vinkla/hashids' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Vinkla\\Hashids\\HashidsServiceProvider',
+    ),
+    'aliases' => 
+    array (
+      'Hashids' => 'Vinkla\\Hashids\\Facades\\Hashids',
+    ),
+  ),
+);
