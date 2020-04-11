9a72fcbc3ff586754afb4054f4d6cc6578fc2766
RaresMihut <63499383+RaresMihut@users.noreply.github.com>
2020-04-11 11:35:45 +0000

    Add files via upload

---
+++ config/languages.php
@@ -0,0 +1,17 @@
+<?php
+
+return [
+    'en' => 'English',
+    'it' => 'Italians',
+    'sp' => 'Spanish',
+    'de' => 'Germans',
+    'pr' => 'Portugeses',
+    'ru' => 'Russians',
+    'pl' => 'Polish',
+    'chi' => 'Mandarin',
+    'hi' => 'Hindi',
+    'fr' => 'French',
+    'fin' => 'Finish',
+    'swe' => 'Swedish',
+
+];
